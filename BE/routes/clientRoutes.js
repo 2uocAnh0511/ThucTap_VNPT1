130ce7d76781a9e72cb028686612a3fa7187434a
quivt4125 <vanquythaicute@gmail.com>
2025-06-15 22:07:48 +0700

    [Client] -  blog
    (cherry picked from commit 137fc880fd4f43760741eb626f905b5715fa2350)

--- BE/routes/clientRoutes.js
+++ BE/routes/clientRoutes.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const categoryController = require('../controllers/Client/categoryController');
+const BlogController = require('../controllers/Client/blogsController');
 
 //------------------[ CLIENT ROUTES ]------------------
 
 //------------------[ CATEGORY ]------------------
 router.get("/category/list", categoryController.getCategories);
+
+//------------------[ Blogs ]------------------
+router.get('/blogs/search', BlogController.searchBlogs);
+router.get('/blogs', BlogController.getAllBlogs);
+router.get('/blogs/:id', BlogController.getBlogById);
+
 module.exports = router;
